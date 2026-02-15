c9350c76b7b68dccc1162229a0241cfc77e64ea3
dvrinda <21f1001205@ds.study.iitm.ac.in>
2026-02-15 10:31:44 +0000

    Enable CORS for submission checker

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import time
 import hashlib
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 # -----------------------------
 # Caches
